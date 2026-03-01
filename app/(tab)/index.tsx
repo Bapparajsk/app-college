74f064fe6a267d2623895208cfb0746bb44e78d7
Bapparaj sk <bapparajsk97@gmail.com>
2026-03-01 16:19:14 +0530

    feat: add pie chart to UserProgress component and restructure data handling

--- app/(tab)/index.tsx
+++ app/(tab)/index.tsx
@@ -1,8 +1,16 @@
 import { Header } from '@/components/header';
 import { ClassProgress, Departments, Library, Service, UserProgress } from '@/components/home';
+
 import { LinearGradient } from "expo-linear-gradient";
 import { ScrollShadow } from 'heroui-native';
 import { ScrollView, View } from 'react-native';
+
+const breakdownData = [
+    { label: 'Monitor', value: 1341, color: '#3b82f6' },   // blue-500
+    { label: 'Laptop', value: 217, color: '#22c55e' },     // green-500
+    { label: 'Mobile', value: 124, color: '#f97316' },     // orange-500
+    { label: 'Tablet', value: 53, color: '#a855f7' },      // purple-500
+];
 
 export default function index() {
     return (
@@ -10,6 +18,7 @@ export default function index() {
             <ScrollView className='p-2' showsVerticalScrollIndicator={false}>
                 <Header />
                 <UserProgress />
+                
                 <ClassProgress />
                 <Library />
                 <Departments />
--- components/home/UserProgress.tsx
+++ components/home/UserProgress.tsx
@@ -1,22 +1,25 @@
 import { Feather, MaterialIcons } from '@expo/vector-icons';
 import { Button, Card, Chip } from 'heroui-native';
-import { useMemo } from 'react';
+// import { PieChart } from 'lucide-react-native';
 import { Text, View } from 'react-native';
-import { LineChart } from "react-native-gifted-charts/dist/LineChart";
+import { PieChart } from "react-native-gifted-charts";
 
 const GraphWidth = 300;
 
+const pieData = [
+    {
+        value: 47,
+        color: '#009FFF',
+        gradientCenterColor: '#006DFF',
+        // focused: true,
+        label: 'Present',
+    },
+    { value: 40, color: '#93FCF8', gradientCenterColor: '#3BE9DE', label: 'Late' },
+    { value: 16, color: '#BDB2FA', gradientCenterColor: '#8F80F3', label: 'Absent' },
+    { value: 3, color: '#FFA5BA', gradientCenterColor: '#FF7F97', label: 'Excused' },
+];
+
 const UserProgress = () => {
-
-    const lineData = [{ value: 100, label: "1" }, { value: 58,  }, { value: 100, label: "3" }, { value: 120, label: "4" }, { value: 150, label: "5" }, { value: 130, label: "6" }, { value: 80, label: "7" }, { value: 100, label: "8" }, { value: 120, label: "9" }, { value: 150, label: "10" }, { value: 130, label: "11" }, { value: 80, label: "12" }, { value: 100, label: "13" }, { value: 120, label: "14" }, { value: 150, label: "15" }, { value: 130, label: "16" }, { value: 80, label: "17" }, { value: 100, label: "18" }, { value: 120, label: "19" }, { value: 150, label: "20" }, { value: 130, label: "21" }, { value: 80, label: "22" }, { value: 100, label: "23" }, { value: 120, label: "24" }, { value: 150, label: "25" }, { value: 130, label: "26" }, { value: 80, label: "27" }, { value: 100, label: "28" }, { value: 120, label: "29" }, { value: 150, label: "30" }];
-    const lineData2 = [{ value: 100, label: "1" }, { value: 20, }, { value: 50, label: "3" }, { value: 80, label: "4" }, { value: 120, label: "5" }, { value: 100, label: "6" }, { value: 60, label: "7" }, { value: 80, label: "8" }, { value: 110, label: "9" }, { value: 140, label: "10" }, { value: 120, label: "11" }, { value: 70, label: "12" }, { value: 90, label: "13" }, { value: 110, label: "14" }, { value: 140, label: "15" }, { value: 120, label: "16" }, { value: 70, label: "17" }, { value: 90, label: "18" }, { value: 110, label: "19" }, { value: 140, label: "20" }, { value: 120, label: "21" }, { value: 70, label: "22" }, { value: 90, label: "23" }, { value: 110, label: "24" }, { value: 140, label: "25" }, { value: 120, label: "26" }, { value: 70, label: "27" }, { value: 90, label: "28" }, { value: 110, label: "29" }, { value: 140, label: "30" }];
-    const lineData3 = [{ value: 100, label: "1" }, { value: 40, }, { value: 40, label: "3" }, { value: 100, label: "4" }, { value: 130, label: "5" }, { value: 110, label: "6" }, { value: 70, label: "7" }, { value: 90, label: "8" }, { value: 120, label: "9" }, { value: 150, label: "10" }, { value: 130, label: "11" }, { value: 80, label: "12" }, { value: 100, label: "13" }, { value: 120, label: "14" }, { value: 150, label: "15" }, { value: 130, label: "16" }, { value: 80, label: "17" }, { value: 100, label: "18" }, { value: 120, label: "19" }, { value: 150, label: "20" }, { value: 130, label: "21" }, { value: 80, label: "22" }, { value: 100, label: "23" }, { value: 120, label: "24" }, { value: 150, label: "25" }, { value: 130, label: "26" }, { value: 80, label: "27" }, { value: 100, label: "28" }, { value: 120, label: "29" }, { value: 150, label: "30" }];
-
-    const spacing = useMemo(() => {
-        const totalSpacing = GraphWidth - 20; // Subtracting padding
-        const spacingValue = totalSpacing / (lineData3.length - 1);
-        return spacingValue;
-    }, [lineData3.length]);
 
     return (
         <Card >
@@ -35,45 +38,54 @@ const UserProgress = () => {
                         </Button>
                     </View>
                 </Card.Header>
-                <Card.Body className="mb-4">
-                    <LineChart
-                        curved
-                        // rotateLabel
-                        isAnimated
-                        data={lineData}
-                        data2={lineData2}
-                        data3={lineData3}
-                        height={80}
-                        width={300}
-                        scrollToEnd
-                        maxValue={200}
-                        spacing={spacing}
-                        initialSpacing={0}
-                        color1="skyblue"
-                        color2="orange"
-                        color3='purple'
-                        noOfSections={3}
-                        yAxisTextStyle={{
-                            fontFamily: 'PoppinsRegular',
-                        }}
-                        xAxisLabelTextStyle={{
-                            fontFamily: 'PoppinsRegular',
-                            fontSize: 12,
-                        }}
-                        xAxisColor="lightgray"
-                        yAxisColor={"lightgray"}
-                        textColor1="green"
-                        hideDataPoints
-                        dataPointsColor1="blue"
-                        dataPointsColor2="red"
-                        dataPointsColor3="purple"
-                        startFillColor1="skyblue"
-                        startFillColor2="orange"
-                        startFillColor3="purple"
-                        startOpacity={0.8}
-                        endOpacity={0.3}
-                    />
-
+                <Card.Body className="mb-4 flex-row">
+                    <View>
+                        <PieChart
+                            data={pieData}
+                            donut
+                            showGradient
+                            isAnimated
+                            ring
+                            edgesRadius={0}
+                            shadowColor="#000000"
+                            radius={90}
+                            innerRadius={60}
+                            innerCircleColor={'#FFFFFF'}
+                            centerLabelComponent={() => {
+                                return (
+                                    <View style={{ justifyContent: 'center', alignItems: 'center', }}>
+                                        <Text
+                                            style={{ fontSize: 22, fontFamily: 'PoppinsSemiBold', lineHeight: 28, color: '#009FFF' }}>
+                                            47%
+                                        </Text>
+                                        <Text style={{ fontSize: 14, fontFamily: 'PoppinsRegular', lineHeight: 20, color: '#6B7280' }}>
+                                            This month
+                                        </Text>
+                                    </View>
+                                );
+                            }}
+                        />
+                    </View>
+                    <View className='grow h-auto' style={{ paddingLeft: 20 }}>
+                        {pieData.map((item, index) => (
+                            <View key={index} className='flex-row items-center justify-between mb-2'>
+                                <View className='flex-row items-center gap-2'>
+                                    <View style={{ backgroundColor: item.color, width: 12, height: 12, borderRadius: 6 }} />
+                                    <Text style={{ fontFamily: 'PoppinsRegular' }}>
+                                        {item.label}
+                                    </Text>
+                                </View>
+                                <View className='flex-row items-center gap-2'>
+                                    <Text style={{ fontFamily: 'PoppinsSemiBold' }}>
+                                        {item.value*2}
+                                    </Text>
+                                    <Text style={{ fontFamily: 'PoppinsSemiBold' }}>
+                                        {item.value}%
+                                    </Text>
+                                </View>
+                            </View>
+                        ))}
+                    </View>
                 </Card.Body>
                 <Card.Footer className="gap-3">
                     <Chip color='danger' variant='soft'>
